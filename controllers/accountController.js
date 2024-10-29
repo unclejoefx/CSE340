0f2d28322507959f4323e8654baa1203621865ae
Uwa Joseph Uwa <uwauwajoe@gmail.com>
2024-10-30 00:56:20 +0100

    accountController.js modified

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -1,6 +1,8 @@
 const utilities = require('../utilities/index'); 
 const accountModel = require('../models/account-model') // Import the account model
-const bcrypt = require("bcryptjs") 
+const bcrypt = require("bcryptjs")
+const jwt = require("jsonwebtoken")
+require("dotenv").config() 
 
 /* ****************************************
 *  Deliver login view
@@ -43,12 +45,13 @@ const processLogin = (req, res) => {
 /* ****************************************
 *  Process Registration
 * *************************************** */
-async function registerAccount(req, res) {
+async function registerAccount(req, res, next) {
     let nav = await utilities.getNav();
-    const { account_firstname,
-            account_lastname,
-            account_email, 
-            account_password    
+    const { 
+        account_firstname,
+        account_lastname,
+        account_email, 
+        account_password    
     } = req.body
 
     // Password sorting
@@ -92,4 +95,64 @@ async function registerAccount(req, res) {
     }
 }
 
-module.exports = { buildLogin, processLogin, buildRegister, registerAccount }
+/* ****************************************
+*  Process Login request
+* *************************************** */
+async function accountLogin(req, res, next) {
+    let nav = await utilities.getNav();
+    const { account_email, account_password } = req.body
+    const accountData = await accountModel.getAccountByEmail(account_email)
+    if (!accountData) {
+        req.flash("notice", "Please check your credentials and try again.")
+        res.status(400).render("account/login", {
+            title: "Login",
+            nav,
+            errors: null,
+            account_email,
+        })
+        return
+    }
+    try {
+        if (await bcrypt.compare(account_password, accountData.account_password)) {
+            delete accountData.account_password
+            const accessToken = jwt.sign(accountData, process.env.ACCESS_TOKEN_SECRET, { expiresIn: 3600 * 1000 })
+            if(process.env.NODE_ENV === "development") {
+                res.cookie("jwt", accessToken, { httpOnly: true, maxAge: 3600 * 1000 })
+            } else {
+                res.cookie("jwt", accessToken, { httpOnly: true, secure: true,  maxAge: 3600 * 1000 })
+            }
+            return res.redirect("/account/")
+        }
+        else {
+            req.flash("message notice", "Please check your credentials and try again.")
+            res.status(400).render("account/login", {
+                title: "Login",
+                nav,
+                errors: null,
+                account_email,
+            })
+        }
+    } catch (error) {
+        throw new Error("Access Forbidden")
+    }
+}
+
+/* ****************************************
+ *  Deliver management view
+ * *************************************** */
+async function getAccountManagement(req, res, next) {
+    try {
+        let nav = await utilities.getNav(); 
+
+        res.render("account/management", {
+            title: "Account Management",
+            nav,
+        });
+    } catch (error) {
+        console.error("Error retrieving account management view:", error);
+        next(error);
+    }
+}
+
+
+module.exports = { buildLogin, processLogin, buildRegister, registerAccount, accountLogin, getAccountManagement}
